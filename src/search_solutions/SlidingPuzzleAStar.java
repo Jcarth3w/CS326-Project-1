d331020549cfdce316cdb140f6786e38deb269b5
Jack Carthew <jdcarthew@bsu.edu>
2024-02-08 18:10:58 -0800

    Add 15 puzzle

--- src/search_solutions/SlidingPuzzleAStar.java
+++ src/search_solutions/SlidingPuzzleAStar.java
@@ -7,14 +7,17 @@ import search_problems.SlidingTilePuzzle;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 public class SlidingPuzzleAStar extends BaseSearch<ArrayList<Integer>, String>
 {
+    private SlidingTilePuzzle a = new SlidingTilePuzzle(new ArrayList<>(List.of(12,1,2,15, 11,6,5,8, 7,10,9,4, 0,13,14,3)), new ArrayList<>(List.of(1,2,3,4, 5,6,7,8 ,9,10,11,12, 13,14,15));
     public SlidingPuzzleAStar()
     {
         super(new SlidingTilePuzzle(),
                 new SortedQueue<>(new CompareMissingTiles(new SlidingTilePuzzle())));
     }
+
 
     public static void main(String[] args) {
         SlidingPuzzleAStar agent = new SlidingPuzzleAStar();
